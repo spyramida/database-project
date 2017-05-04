f1ec9a2a1fc488280df734f9774ac4a0f4919ab2
spyramida <spyros-papp@hotmail.com>
2017-05-05 01:23:38 +0300

    Add files via upload

---
+++ vaseis_su/process_pay.php
@@ -0,0 +1,28 @@
+<head >
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="bootstrap.min.css">
+<script src="jquery.min.js"></script>
+<script src="bootstrap.min.js"></script>
+
+<link rel="stylesheet" href="jquery-ui.css">
+<script src="jquery-1.10.2.js"></script>
+<script src="jquery-ui.js"></script>
+<script>
+$(function() {
+  $( "#datepicker" ).datepicker();
+});
+</script>
+
+</head>
+
+aaaa
+<body style="background-image:url(main.jpg); ">
+
+
+
+</body>
+
+
+</html>
